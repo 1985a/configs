596124ade89e45b860fc284d61b7038fefae3caa
1985a <d_wagner_pro@hotmail.com>
2017-02-06 20:44:51 -0400

    Add dir_colors file

--- scripts/spoti.sh
+++ scripts/spoti.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 title=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
 artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|cut -b 20-|cut -d '"' -f 2|egrep -v ^$|egrep -v array|egrep -v artist`
 echo  $artist '-' $title
